5982f01fe5a7828ab90ad3db3f28765453edbafd
warley mendes <warleymendes.dev@gmail.com>
2023-04-03 20:22:27 -0300

    prod

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
